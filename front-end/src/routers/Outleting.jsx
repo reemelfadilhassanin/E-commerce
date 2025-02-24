4ce0baac8a697a58bf5350809aaf4e3c455719dd
Mostafa-Sukhnia <mustafa.2005.sukhnia@gmail.com>
2025-02-24 18:22:55 +0300

    rename the front end to frontend

--- front-end/src/routers/Outleting.jsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router"
-
-function Outleting() {
-  return (
-    <Outlet/>
-  )
-}
-
-export default Outleting
--- front-end/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import signin from './slices/signin'
-
-export const store = configureStore({
-  reducer: {
-    signin,
-  },
-})
