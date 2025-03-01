421418ceda6e1602aa332ecbca1644a295735e8b
Mostafa-Sukhnia <mustafa.2005.sukhnia@gmail.com>
2025-03-01 23:39:50 +0300

    debug th conflict

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router"
-import { Header } from "./exporting"
-import Footer from "../components/ui/Footer"
+// import { Header } from "./exporting"
+// import Footer from "../components/ui/Footer"
 function Home() {
   return (
     <div className="flex flex-col">
-        {/* <Link to="signin">signIn</Link>
-        <Link to="signup">signUp</Link> */}
-        <Header/>
+        <Link to="signin">signIn</Link>
+        <Link to="signup">sinUp</Link>
+        {/* <Header/> */}
     </div>
   )
 }
